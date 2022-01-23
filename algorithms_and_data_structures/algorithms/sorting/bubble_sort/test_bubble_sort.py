03415731b057f8cede27b425df0ec267551ab2bd
JCPedroza <nemesis4go10@hotmail.com>
2022-01-22 21:26:55 -0600

    fix: create new test cases for each algorithm

--- algorithms_and_data_structures/algorithms/sorting/bubble_sort/test_bubble_sort.py
+++ algorithms_and_data_structures/algorithms/sorting/bubble_sort/test_bubble_sort.py
@@ -1,9 +1,11 @@
 from . import bubble_sort
 from . import bubble_sort_optimized
+from . import bubble_sort_recursive
 
 test_subjects = [
     bubble_sort,
-    bubble_sort_optimized
+    bubble_sort_optimized,
+    bubble_sort_recursive
 ]
 
 
